183e33de3af6721110325c9d6d8d076494c4617e
Isaque Pestana <pestanaisaque@gmail.com>
2017-10-23 13:09:23 -0200

    Logica da busca de medicamento por Nome Comercial implementada. Tela pendente.

--- src/main/java/br/umc/sgmed/dao/MedicamentoDAO.java
+++ src/main/java/br/umc/sgmed/dao/MedicamentoDAO.java
@@ -3,13 +3,17 @@ package br.umc.sgmed.dao;
 import java.util.List;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import br.umc.sgmed.po.MedicamentoPO;
 
 @Repository("medicamentoRepository")
 public interface MedicamentoDAO extends JpaRepository<MedicamentoPO, Integer> {
-	List<MedicamentoPO> findAllByNomeComercial(String nomeComercial);
+	
+	@Query("SELECT m FROM MedicamentoPO m WHERE UPPER(m.nomeComercial) LIKE CONCAT('%', UPPER(:nomeComercial), '%')")
+	List<MedicamentoPO> findAllByNomeComercial(@Param("nomeComercial") String nomeComercial);
 
 	MedicamentoPO findByPrincipioAtivo(String principioAtivo);
 }
