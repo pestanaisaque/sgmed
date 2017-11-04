b93008d3ed4ea99f724cd44ec3a6904e5f58a801
Isaque Pestana <pestanaisaque@gmail.com>
2017-11-04 15:54:47 -0200

    Correcao para que seja possivel inserir o id do medicamento

--- src/main/java/br/umc/sgmed/po/MedicamentoPO.java
+++ src/main/java/br/umc/sgmed/po/MedicamentoPO.java
@@ -17,7 +17,7 @@ import org.springframework.web.bind.annotation.ModelAttribute;
 public class MedicamentoPO {
 
 	@Id
-	@GeneratedValue(strategy = GenerationType.AUTO)
+//	@GeneratedValue(strategy = GenerationType.AUTO)
 	@Column(name = "medicamento_id")
 	private Integer idMedicamento;
 	
